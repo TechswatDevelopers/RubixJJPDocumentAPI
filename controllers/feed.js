2545d01c51c747387ec0d3f2c10b61eb330b3040
Xraycat07 <mikkiedutoit@gmail.com>
2021-10-04 15:25:28 +0200

    comments

--- controllers/feed.js
+++ controllers/feed.js
@@ -61,22 +61,24 @@ exports.createPost = (req, res, next) => {
 };
 
 exports.getPost = (req, res, next) => {
-      const postId = req.params.postId;
-      Post.findById(postId)
-          .then(post => {
-            if (!post){
-              const error = new Error('Could not find post.')
-              error.statusCode = 404;
-              throw error;
-            }
-            res.status(200).json({ message: 'Post fetched.', post: post });
-          })
-          .catch(err => {
-            if (!err.statusCode){
-                err.statusCode = 500;
-            }
-            next(err);
-          });
+  const postId = req.params.postId;
+  // const imgArray= res.map(element => element.postId)
+  // res.send(imgArray)
+  Post.findById(postId)
+      .then(post => {
+        if (!post){
+          const error = new Error('Could not find post.')
+          error.statusCode = 404;
+          throw error;
+        }
+        res.status(200).json({ message: 'Post fetched.', post: post });
+      })//, buffer: send(res.image.buffer)
+      .catch(err => {
+        if (!err.statusCode){
+            err.statusCode = 500;
+        }
+        next(err);
+      });
   };
 
   exports.updatePost = (req, res, next) => {
